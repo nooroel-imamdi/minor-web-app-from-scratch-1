12464e1c66eb50855a8cfadaab030c8997fe351b
Lars Douwe Schuitema <info@larsschuitema.nl>
2017-02-08 15:59:31 +0100

    add active to menu and update desktop view

--- opdracht5/static/app.js
+++ opdracht5/static/app.js
@@ -22,8 +22,10 @@
       sectionsList.forEach(function(section) {
         if(location.hash === "#" + section.id) {
           section.classList.add("active");
+          document.querySelector("a[href='#" + section.id + "']").classList.add("active");
         } else {
           section.classList.remove("active");
+          document.querySelector("a[href='#" + section.id + "']").classList.remove("active");
         }
       });
     }
