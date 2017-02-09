cb529b7dd2df29d71b976a444eadc89bc61d2cbd
Lars Douwe Schuitema <info@larsschuitema.nl>
2017-02-09 17:02:55 +0100

    update geo.js with es6

--- geo/cmdgeo-0.2.js
+++ geo/cmdgeo-0.2.js
@@ -1,7 +1,7 @@
 "use strict";
 
-(function() {
-  var app = {
+{ // ES6 Block global scope
+  const app = {
 
     gps: {
       SANDBOX: "SANDBOX",
@@ -15,40 +15,40 @@
       ET: new EventTarget():  
     },
 
-    eventTarget: function() {
+    eventTarget: () => {
       this._listeners = {}
     },
 
-    init: function() {
+    init: () => {
       debugMessage("Controleer of GPS beschikbaar is...");
 
       gps.ET.addListener(GPS_AVAILABLE, startInterval);
-      gps.ET.addListener(GPS_UNAVAILABLE, function(){debugMessage('GPS is niet beschikbaar.')});
+      gps.ET.addListener(GPS_UNAVAILABLE, () => {debugMessage('GPS is niet beschikbaar.')});
 
       (geo_position_js.init())?ET.fire(GPS_AVAILABLE):ET.fire(GPS_UNAVAILABLE);
     },
 
-    startInterval: function() {
+    startInterval: () => {
       debugMessage("GPS is beschikbaar, vraag positie.");
       updatePosition();
       interval = self.setInterval(updatePosition, REFRESH_RATE);
       ET.addListener(POSITION_UPDATED, checkLocations);
     },
 
-    updatePosition: function() {
+    updatePosition: () => {
       intervalCounter++;
       geo_position_js.getCurrentPosition(_set_position, _geo_error_handler, {enableHighAccuracy:true});
     },
 
-    setPosition: function() {
+    setPosition: () => {
       currentPosition = position;
       gps.ET.fire("POSITION_UPDATED");
       debugMessage(intervalCounter+" positie lat:"+position.coords.latitude+" long:"+position.coords.longitude);
     },
 
-    checkLocations: function() {
+    checkLocations: () => {
       // Liefst buiten google maps om... maar helaas, ze hebben alle coole functies
-      for (var i = 0; i < locaties.length; i++) {
+      for (let i = 0; i < locaties.length; i++) {
           locatie: {
             coords: {
               latitude: locaties[i][3],
@@ -76,7 +76,7 @@
       }
     },
 
-    calculateDistance: function() {
+    calculateDistance: () => {
       positions: {
         pos1: new google.maps.LatLng(p1.coords.latitude, p1.coords.longitude,
         pos2: new google.maps.LatLng(p2.coords.latitude, p2.coords.longitude
@@ -84,29 +84,29 @@
       return Math.round(google.maps.geometry.spherical.computeDistanceBetween(positions.pos1, positions.pos2), 0);
     },
 
-    generateMap: function() {
+    generateMap: () => {
       debugMessage("Genereer een Google Maps kaart en toon deze in #"+canvasId)
       map = new google.maps.Map(document.getElementById(canvasId), myOptions);
 
-      var routeList = [];
+      const routeList = [];
       debugMessage("Locaties intekenen, tourtype is: "+tourType);
-      for (var i = 0; i < locaties.length; i++) {
+      for (let i = 0; i < locaties.length; i++) {
           try {
               (localStorage.visited==undefined||isNumber(localStorage.visited))?localStorage[locaties[i][0]]=false:null;
           } catch (error) {
               debugMessage("Localstorage kan niet aangesproken worden: "+error);
           }
 
-          var markerLatLng = new google.maps.LatLng(locaties[i][3], locaties[i][4]);
+          const markerLatLng = new google.maps.LatLng(locaties[i][3], locaties[i][4]);
           routeList.push(markerLatLng);
 
           markerRij[i] = {};
-          for (var attr in locatieMarker) {
+          for (let attr in locatieMarker) {
               markerRij[i][attr] = locatieMarker[attr];
           }
           markerRij[i].scale = locaties[i][2]/3;
 
-          var marker = new google.maps.Marker({
+          const marker = new google.maps.Marker({
               position: markerLatLng,
               map: map,
               icon: markerRij[i],
@@ -116,7 +116,7 @@
 
       if(tourType == LINEAIR){
           debugMessage("Route intekenen");
-          var route = new google.maps.Polyline({
+          const route = new google.maps.Polyline({
               clickable: false,
               map: map,
               path: routeList,
@@ -137,25 +137,25 @@
       gps.ET.addListener(POSITION_UPDATED, updatePosition);
     },
 
-    isNumber: function(n) {
+    isNumber: n => {
     return !isNaN(parseFloat(n)) && isFinite(n);
     }
 
-    geoErrorHandler: function(code, message) {
+    geoErrorHandler: (code, message) => {
       debug_message('geo.js error '+code+': '+message);
     }
 
-    debugMessage: function(event) {
+    debugMessage: event => {
       // use currentPosition to center the map
-      var newPos = new google.maps.LatLng(currentPosition.coords.latitude, currentPosition.coords.longitude);
+      const newPos = new google.maps.LatLng(currentPosition.coords.latitude, currentPosition.coords.longitude);
       map.setCenter(newPos);
       currentPositionMarker.setPosition(newPos);
     },
 
-    setCustomDebugging: function() {
+    setCustomDebugging: () => {
       debugId = this.debugId;
       customDebugging = true;
     }
 
   };
-})
+}
