57775b29f5fc49a58b2cd6e8877f78d7c2620514
Lars Douwe Schuitema <info@larsschuitema.nl>
2017-02-09 16:21:00 +0100

    update template literals to es6

--- opdracht5/static/app.js
+++ opdracht5/static/app.js
@@ -22,12 +22,12 @@
       const sectionsList = document.querySelectorAll("section");
 
       sectionsList.forEach(section => {
-        if(route === "#" + section.id) {
+        if(route === `#${section.id}`) {
           section.classList.add("active");
-          document.querySelector("a[href='#" + section.id + "']").classList.add("active");
+          document.querySelector(`a[href='#${section.id}']`).classList.add("active");
         } else {
           section.classList.remove("active");
-          document.querySelector("a[href='#" + section.id + "']").classList.remove("active");
+          document.querySelector(`a[href='#${section.id}']`).classList.remove("active");
         }
       });
     }
